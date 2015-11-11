02a7af2fc28a9ca731020066af9349b03dc85a96
stigsdk <stig@sornsen.net>
2015-11-11 13:08:15 +0100

    Models changes

--- Skemaplanlægning/Models/SprintInstance.cs
+++ Skemaplanlægning/Models/SprintInstance.cs
@@ -1,7 +1,11 @@
-﻿namespace Models
+﻿using System.Collections.ObjectModel;
+
+namespace Models
 {
     public class SprintInstance
     {
-         
+        public int Id { get; set; }
+        public Sprint SprintInstance { get; set; }
+        public ObservableCollection< MyProperty { get; set; }
     }
 }
