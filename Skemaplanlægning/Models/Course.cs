8435f6a2ebea33b58bf433ff2506bddd9da36900
Jesperb21 <Jesper.j.b.baunsgaard@gmail.com>
2015-11-11 12:49:43 +0100

    Added fields in Teacher & Course models

--- Skemaplanlægning/Models/Course.cs
+++ Skemaplanlægning/Models/Course.cs
@@ -2,6 +2,19 @@
 {
     public class Course
     {
-         
+        /// <summary>
+        /// Course Id
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name of the course
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Duration of the course in weeks
+        /// </summary>
+        public int Duration { get; set; }
     }
 }
--- Skemaplanlægning/Models/Teacher.cs
+++ Skemaplanlægning/Models/Teacher.cs
@@ -1,7 +1,27 @@
-﻿namespace Models
+﻿using System.Collections.Generic;
+
+namespace Models
 {
     public class Teacher
     {
-         
+        /// <summary>
+        /// the unique identifier of the specific teacher
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// FirstName of the teacher
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// LastName of the teacher
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// A list of courses the Teacher is eligable to teach in
+        /// </summary>
+        public List<Course> Skills { get; set; }
     }
-}
+}   
